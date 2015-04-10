7db2f4c3f2dc32ca743dcf6e7293f53188ed1e2c
frederic <frederic.teissier@live.fr>
2015-04-10 13:59:52 +0200

    create user entity and repository add routes

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,9 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Applisun\AireJeuxBundle\ApplisunAireJeuxBundle(),
+            // Bundles additionnel
+            new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src/Applisun/AireJeuxBundle/Entity/User.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Applisun\AireJeuxBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use FOS\UserBundle\Model\User as FOSUser;
+
+/**
+ * User
+ *
+ * @ORM\Table(name="user")
+ * @ORM\Entity(repositoryClass="Applisun\AireJeuxBundle\Repository\UserRepository")
+ */
+class User extends FOSUser
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
+
